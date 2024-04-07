a26ee971f3c73d5e3553b537395dcf40d316b27f
JoyRume <joy.rume@student.moringaschool.com>
2024-04-07 11:24:56 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,19 +1,22 @@
-from flask import Flask, jsonify
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-
 @app.route('/')
-def home():
-    return '<h1>BAKERY BUILDING GET API LAB</h1>'
-
+def index():
+    return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def get_bakeries():
